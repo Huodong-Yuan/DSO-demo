8169f320dc9e77b43eb7056f99b0afb863ac1bdb
Huodong Yuan <huodong.yuan@hotmail.com>
2021-02-21 15:10:52 +0800

    fix errors

--- script/test-scan-result.sh
+++ script/test-scan-result.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
-isHigh=$(aws ecr describe-image-scan-findings --repository-name dso-repo --image-id imageTag=1.0 --region ap-east-1 | grep "\"severity\": \"HIGH\"");
+set -e
+die () {
+    echo >&2 "$@"
+    exit 1
+}
+
+### Expect argument to be provided with the ECR repository name
+[ "$#" -eq 1 ] || die "Usage: $0 [ecr_repo]"
+ECR_REPO=$1
+
+isHigh=$(aws ecr describe-image-scan-findings --repository-name $ECR_REPO --image-id imageTag=1.0 --region ap-east-1 | grep "\"severity\": \"HIGH\"");
 
 if [ -z "$isHigh" ]; then 
     echo "Not high"
