1efa88186e5d0e9390f35fa95fb2bb9a540d85fe
Huodong Yuan <huodong.yuan@hotmail.com>
2021-02-22 01:32:41 +0800

    ......

--- script/create-ecr.sh
+++ script/create-ecr.sh
@@ -17,5 +17,5 @@ if aws cloudformation describe-stacks --stack-name $STACK_NAME; then
     exit 0
 else
     echo "Stack with id $STACK_NAME does not exist. Create one." 
-    aws cloudformation create-stack --stack-name ecr-repo --template-url https://cf-templates-i6aohmzkcsjf-ap-east-1.s3.ap-east-1.amazonaws.com/templates/ecr-repository.yaml
+    aws cloudformation create-stack --stack-name $STACK_NAME --template-url https://cf-templates-i6aohmzkcsjf-ap-east-1.s3.ap-east-1.amazonaws.com/templates/ecr-repository.yaml
 fi
