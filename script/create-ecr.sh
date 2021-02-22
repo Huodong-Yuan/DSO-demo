839723c24ea7b8459af0be9ebb69095d7259973a
Huodong Yuan <huodong.yuan@hotmail.com>
2021-02-22 11:40:45 +0800

    update stack for ecr

--- script/create-ecr.sh
+++ script/create-ecr.sh
@@ -15,9 +15,10 @@ aws sts get-caller-identity
 ### Check if stack exists yet
 echo Checking if stack $STACK_NAME exists yet
 if aws cloudformation describe-stacks --stack-name $STACK_NAME; then
-    echo "Stack with id $STACK_NAME found. Continue."
+    echo "Stack with id $STACK_NAME found. Updating stack..."
+    aws cloudformation update-stack --stack-name $STACK_NAME --template-body ./infrastructure/ecr-repository.yaml
     exit 0
 else
     echo "Stack with id $STACK_NAME does not exist. Create one." 
-    aws cloudformation create-stack --stack-name $STACK_NAME --template-url https://cf-templates-i6aohmzkcsjf-ap-east-1.s3.ap-east-1.amazonaws.com/templates/ecr-repository.yaml
+    aws cloudformation create-stack --stack-name $STACK_NAME --template-body ./infrastructure/ecr-repository.yaml
 fi
