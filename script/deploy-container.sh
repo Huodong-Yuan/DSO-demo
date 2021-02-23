392581ce699ad6db1eb075d897059cb2ac9ccbad
Huodong Yuan <huodong.yuan@hotmail.com>
2021-02-23 09:26:14 +0800

    refine.

--- script/deploy-container.sh
+++ script/deploy-container.sh
@@ -33,6 +33,5 @@ if [ -z "$HAS_SERVICE" ]; then
     kubectl expose deployment $SERVICE --type=LoadBalancer --port 80 --target-port 80
 else 
     echo "Service exists, continue to deploy container"
+    kubectl set image deployment/$SERVICE $IMAGE_REPO_NAME=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME:$IMAGE_TAG
 fi
-
-kubectl set image deployment/$SERVICE $IMAGE_REPO_NAME=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME:$IMAGE_TAG
