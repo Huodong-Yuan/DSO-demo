3a64abd56503c7c1fafd9871fcbc52a036737da2
Huodong Yuan <huodong.yuan@hotmail.com>
2021-02-23 17:11:39 +0800

    An image does not exist locally with the tag: 937127767762.dkr.ecr.ap-east-1.amazonaws.com/ecr-repo

--- script/build-container.sh
+++ script/build-container.sh
@@ -15,7 +15,7 @@ else
     exit 0
 fi
 
-docker build -t $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/demo:$IMAGE_TAG .
+docker build -t $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME:$IMAGE_TAG .
 docker tag $IMAGE_REPO_NAME:$IMAGE_TAG $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME:$IMAGE_TAG
 
 aws ecr get-login-password --region $AWS_DEFAULT_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
