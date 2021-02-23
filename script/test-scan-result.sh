1cdced576e0bbe8b5d9759b177d8304136ebd4a4
Huodong Yuan <huodong.yuan@hotmail.com>
2021-02-23 10:34:54 +0800

    checking image existing before scanning

--- script/test-scan-result.sh
+++ script/test-scan-result.sh
@@ -11,6 +11,14 @@ die () {
 ECR_REPO=$1
 IMAGE_TAG=$2
 
+echo Checking if ECR repository $ECR_REPO exists yet
+if aws ecr describe-images --repository-name $ECR_REPO --image-ids imageTag=$IMAGE_TAG; then
+    echo "Image tage $IMAGE_TAG in ECR repository $ECR_REPO found. Continue..."
+else
+    echo "Image tage $IMAGE_TAG in ECR repository $ECR_REPOdoes not exist. exit 0 without scanning." 
+    exit 0
+fi
+
 isHigh=$(aws ecr describe-image-scan-findings --repository-name $ECR_REPO --image-id imageTag=$IMAGE_TAG --region ap-east-1 | grep "\"severity\": \"HIGH\"");
 
 if [ -z "$isHigh" ]; then 
