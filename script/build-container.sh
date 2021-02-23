2fc68e0d000985d6659d38845e89841c347bd95b
Huodong Yuan <huodong.yuan@hotmail.com>
2021-02-23 16:06:57 +0800

    login to ecr before pushing docker image to it

--- script/build-container.sh
+++ script/build-container.sh
@@ -18,5 +18,7 @@ fi
 
 docker build -t $IMAGE_REPO_NAME:$IMAGE_TAG .
 docker tag $IMAGE_REPO_NAME:$IMAGE_TAG $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME:$IMAGE_TAG
+
+aws ecr get-login-password --region $AWS_DEFAULT_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
 docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME:$IMAGE_TAG
 sleep 1m
